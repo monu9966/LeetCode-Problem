ef1339afeee543878f59f4bba847b945346b3330
monu9966 <monukumar896914@gmail.com>
2025-12-09 17:37:04 +0530

    roman to integer problem

---
+++ 13_roman_To_Integer.py
@@ -0,0 +1,27 @@
+class Solution:
+    def romanToInteger(self, s: str) -> int:
+        roman = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+        }
+
+        n = len(s)
+        result = 0
+
+        for i in range(n):
+            if i + 1 < 0 and roman[s[i]] < roman[s[i + 1]]:
+                result -= roman[s[i]]
+
+            else:
+                result += roman[s[i]]
+        
+        return result
+
+sol = Solution()
+# print(sol.romanToInteger(input("Enter a Roman numeral: ")))
+print(f"The integer of roman numeral is: {sol.romanToInteger(input("Enter a Roman numeral: "))}")
